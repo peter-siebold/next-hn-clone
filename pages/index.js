9d3129735e1915fc5bde7856defde89ab0195f2e
Peter Siebold <Peter.Siebold@unitedplanet.de>
2019-02-11 16:56:22 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import StoryList from "../components/StoryList";
+import Layout from "../components/Layout";
+
+class Index extends React.Component {
+    static async getInitialProps() {
+        let stories;
+        try {
+            const res = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+            stories = await res.json();
+            console.log(stories)
+        } catch (err) {
+            console.log(err);
+            stories = [];
+        }
+        return { stories };
+    }
+    render() {
+        const {stories} = this.props;
+        if(stories.length === 0){
+            return <Error statusCode={503} />
+        }
+        return (
+            <Layout title="HN Next" description="A Hacker News clone made with Next.js">
+                <StoryList stories={stories} />
+            </Layout>
+        )
+    }
+}
+
+export default Index;
